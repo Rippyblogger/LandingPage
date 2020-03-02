eff60161e1dfe5b6f8fd137d0697262f7ada98af
Boye Adeotan <55053080+Rippyblogger@users.noreply.github.com>
2020-03-02 22:03:31 +0100

    Fixed overflowing css and issues of multiple active classes.
    1. Added fourth section to index.html
    2. Fixed overflowing UL nav on small screens.
    3.Fixed issue of mutiple active classes on sections even when not in viewport.

--- js/app.js
+++ js/app.js
@@ -19,63 +19,60 @@
  */
 //Get list of all sections, declare the first unordered list to be used as a container for li nodes later.
 const sections = document.querySelectorAll("section");
+console.log(sections);
 const uls = document.querySelector("ul");
 currentSection = document.querySelector(`[data-nav="${this.id}"]`);
 
-
 function newClass(sectionsList) {
-    //console.log(sectionsList.classList);
-    sectionsList.classList.add("active");
+  //console.log(sectionsList.classList);
+  sectionsList.classList.add("active");
 }
+//logic borrowed from https://gist.github.com/jjmu15/8646226
+const isInViewport = function(section) {
+  var rect = section.getBoundingClientRect();
+  console.log(rect.top, rect.left, rect.bottom, rect.right, window.innerHeight);
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight + 150 || document.documentElement.clientHeight) &&
+    rect.right <=
+      (window.innerWidth + 150 || document.documentElement.clientWidth)
+  );
+};
+
 //loop through all the sections, get data attribute, use as list text
 for (let i = 0; i < sections.length; i++) {
-    const section = sections[i];
-    let sectionData = sections[i].getAttribute("data-nav");
-    const sectionsList = document.createElement("li");
-    sectionsList.setAttribute("id", sectionData);
-    sectionsList.textContent = sectionData;
-    sectionsList.innerHTML = `<a onClick="newClass" href="#${sections[i].id}">${sections[i].id}</a>`;
+  const section = sections[i];
+  let sectionData = section.getAttribute("data-nav");
+  const sectionsList = document.createElement("li");
+  sectionsList.setAttribute("id", sectionData);
+  sectionsList.textContent = sectionData;
+  sectionsList.innerHTML = `<a onClick="newClass" href="#${section.id}">${section.id}</a>`;
 
+  
 
-    //logic borrowed from https://gist.github.com/jjmu15/8646226
-    const isInViewport = function(section) {
-        var rect = currentSection.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || html.clientHeight) &&
-            rect.right <= (window.innerWidth || html.clientWidth)
-        );
-    };
+  //test for presence of active class in all li elements. If present, remove from sibling elezments.
 
+  sectionsList.onclick = function() {
+    let test = document.querySelector(".active");
+    if (test !== null) {
+      test.classList.remove("active");
+    }
 
-    //test for presence of active class in all li elements. If present, remove from sibling elezments.
+    newClass(this);
+  };
+  // on scroll add active class and remove from sections not in viewport
+  
 
-
-    sectionsList.onclick = function() {
-        let test = document.querySelector('.active');
-        if (test !== null) {
-            test.classList.remove('active');
-        }
-
-
-        newClass(this);
-
-
-    };
-    // on scroll add active class and remove from sections not in viewport
-    document.addEventListener("scroll", () => {
-        for(const sec in sections){
-            if isInViewport(currentSection){
-                section.classList.add("active");
-            }else{
-                section.classList.remove("active");
-            }
-        }
-        
-      });
-
-    uls.appendChild(sectionsList);
-
+  uls.appendChild(sectionsList);
 }
-
+document.addEventListener("scroll", () => {
+    for (let index = 0; index < sections.length; index++) {
+      if (isInViewport(sections[index])) {
+        sections[index].classList.add("active");
+      } else {
+        sections[index].classList.remove("active");
+      }
+    }
+  });
